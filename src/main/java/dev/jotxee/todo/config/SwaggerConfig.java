1dd538440054426c6da955bf44eaefc5ecd7c4c2
Jose <joselitosbd@gmail.com>
2026-05-01 20:00:50 +0200

    feat: add async email sending and JWT Bearer auth in Swagger
    EmailService.sendOtp() now runs in a dedicated mailExecutor thread pool
    so HTTP responses return immediately. Swagger UI exposes an Authorize
    button to send Bearer tokens on protected endpoints.

--- src/main/java/dev/jotxee/todo/config/SwaggerConfig.java
+++ src/main/java/dev/jotxee/todo/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package dev.jotxee.todo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,9 +25,16 @@ public class SwaggerConfig {
                 .info(new Info().title("TODO API")
                         .description("Documentación de API de Todo App")
                         .version("v1.0"))
-                .servers(List.of(new Server().url(baseUrl)))  // Usar la URL dinámica
+                .servers(List.of(new Server().url(baseUrl)))
                 .externalDocs(new ExternalDocumentation()
                         .description("Documentación completa")
-                        .url(baseUrl + "/swagger-ui.html"));  // Configurar la URL de Swagger UI
+                        .url(baseUrl + "/swagger-ui.html"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .name("Bearer")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
